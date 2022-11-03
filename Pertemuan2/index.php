d915843a0a5ac43323fcbc257812fc1cf2fd15fe
unknown <yusronanshori14@gmail.com>
2022-11-02 19:36:25 -0700

    Kuliah Hari ini 03/11/22

---
+++ Pertemuan2/index.php
@@ -0,0 +1,33 @@
+<?php 
+// standar output di php
+//echo, print, print-f, var_dump
+
+
+$nama = "Ahmad Yusron";
+echo "Hallo, Nama saya $nama";
+
+// Operator
+// aritmatika
+// * / + - %
+
+$x = 10;
+$y = 20;
+echo $x + $y
+
+// Penggabunagan string / concatenation / concat
+// .
+
+$nama_depan = "Ahmad";
+$nama_belakang = "Yusron";
+echo $nama_depan . " " . $nama_belakang;
+
+// assigment
+// =,+=,-=,*=, /=, %=, .=
+$x = 1;
+$x /= 5;
+echo $x;
+$nama .= $nama_depan;
+$nama .= " ";
+$nama .= $nama_belakang;
+echo $nama;
+?>
